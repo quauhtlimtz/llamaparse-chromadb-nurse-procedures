d4025afffc342b6bef4868962edcfc58a4b22c3f
Quauhtli Martinez <quauhtli.martinez@eninetworks.com>
2024-06-01 11:39:13 -0600

    Ignore Storage folder

--- main.py
+++ main.py
@@ -124,5 +124,5 @@ def search_query(query: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="0.0.0.0", port=8000)
